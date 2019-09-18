e1d796c2045068770f177d3b194e4cc25cfbe613
Agnieszka <agaco@gazeta.pl>
2019-09-18 22:20:36 +0200

    get endpoint

--- app/public/zadanieDnia/js/front.js
+++ app/public/zadanieDnia/js/front.js
@@ -1,1 +1,19 @@
-// Twój kod
+window.addEventListener('load', () => {
+ console.log('fdsfsdf')
+})
+
+$(function(){
+
+ fetch('/getList', {
+  method : 'GET',
+  // body : JSON.stringify({
+  //  name : 'Imię',
+  //  surname : 'Nazwisko',
+  // }),
+  headers: {
+   'Content-Type': 'application/json',
+  }
+ })
+.then(res => console.log(res))
+
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,43 @@
-//Twój kod
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true})); //uzywaj parsowania do body
+app.use(bodyParser.json());
+app.use(express.static('./public/zadanieDnia/'));
+
+///////////////////
+const dbPath = '../app/data/przykladOdczytZapis/db.json'
+//////////////////
+
+app.get('/getList', (req, res) => {
+ fs.readFile(dbPath, (err, data) => {
+  if (!err){
+   const shoppingList = JSON.parse(data);
+   res.send(shoppingList);
+  } else {
+   console.log('Błąd odczytu pliku', err);
+   res.send('Wystąpił błąd odczytu.');
+  }
+ });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//wlacz nasłuchiwanie
+app.listen(3000, () => {
+ console.log('Serwer uruchomiony na porcie 3000');
+});
